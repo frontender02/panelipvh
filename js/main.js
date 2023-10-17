e893e24207c130049835d33a395ca1d79dc79525
frontender02 <VaRVaRaNeVa@yandex.ru>
2023-10-17 16:12:21 +0300

    header and footer

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body
+
+    // Menu
+    const menu = document.querySelector('.menu__body')
+	const menuBtn = document.querySelector('.menu__icon')
+	if (menu && menuBtn) {
+		menuBtn.addEventListener('click', () => {
+			menu.classList.toggle('active')
+			menuBtn.classList.toggle('active')
+			body.classList.toggle('lock')
+		})
+		menu.querySelectorAll('.menu__link').forEach(link => {
+			link.addEventListener('click', () => {
+				menu.classList.remove('active')
+				menuBtn.classList.remove('active')
+				body.classList.remove('lock')
+			})
+		})
+	}
+})
