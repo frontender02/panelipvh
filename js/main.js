c4fd0692da75b2eb86540fa3f4328968c9b8805c
frontender02 <VaRVaRaNeVa@yandex.ru>
2023-10-18 12:04:48 +0300

    home profit banner

--- js/main.js
+++ js/main.js
@@ -18,4 +18,17 @@ document.addEventListener('DOMContentLoaded', () => {
 			})
 		})
 	}
+
+	// Slider
+	const swiperHome = new Swiper('.home__slider', {
+		// loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	})
 })
