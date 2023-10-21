18990a484b401dc67710c727344159f6206f1e5c
frontender02 <VaRVaRaNeVa@yandex.ru>
2023-10-21 11:07:53 +0300

    application tabs cards

--- js/main.js
+++ js/main.js
@@ -19,6 +19,40 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 
+	// Tabs
+	function showTabs(btn, body) {
+		const tabsBtn = document.querySelectorAll(btn)
+		const tabsContent = document.querySelectorAll(body)
+
+		if (tabsContent.length > 0 || tabsBtn.length > 0) {
+			function hideTabContent() {
+				tabsContent.forEach(item => {
+					item.classList.remove('active')
+				})
+
+				tabsBtn.forEach(item => {
+					item.classList.remove('active')
+				})
+			}
+
+			function showTabContent(i = 0) {
+				tabsContent[i].classList.add('active')
+				tabsBtn[i].classList.add('active')
+			}
+
+			hideTabContent()
+			showTabContent()
+
+			tabsBtn.forEach((tab, index) => {
+				tab.addEventListener('click', () => {
+					hideTabContent()
+					showTabContent(index)
+				})
+			})
+		}
+	}
+	showTabs('.tabs-application__btn', '.tab-application')
+
 	// Slider
 	const swiperHome = new Swiper('.home__slider', {
 		// loop: true,
